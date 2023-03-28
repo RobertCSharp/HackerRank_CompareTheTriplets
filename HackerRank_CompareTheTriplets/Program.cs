70f6adcba78b05fede978ca19267c3cb6fa4eec9
Robert Stefan <rpahonie@yahoo.com>
2023-03-28 20:59:50 +0200

    Add project files.

---
+++ HackerRank_CompareTheTriplets/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank_CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ",CompareTriplets (new List<int>(){ 17, 28, 30 }, new List<int>(){ 99, 16, 18 })));
+        }
+
+        public static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            List<int> result = new List<int>();
+
+            int counterAlice = 0;
+            int counterBob = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    counterAlice++;
+                }
+
+                if (b[i] > a[i])
+                {
+                    counterBob++;
+                }
+            }
+
+            result.Add(counterAlice);
+            result.Add(counterBob);
+
+            return result;
+        }
+    }
+}
